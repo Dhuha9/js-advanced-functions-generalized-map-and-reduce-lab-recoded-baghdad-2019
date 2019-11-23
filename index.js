1e4ec94a64b9840125f07f0b567ca8f145d154bf
Dhuha Hasan <duha_9@yahoo.com>
2019-11-23 10:04:00 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Add your functions here
+map([1, 2, 3, -9], function(a){ return a * a })
+
+function map(sourceArray,fun){
+    let newArray=[];
+    for(let i=0;i<sourceArray.length;i++){
+        newArray.push(fun(sourceArray[i]));
+    }
+    return newArray;
+}
+
+
+function reduce(sourceArray,fun,startingPoint){
+ 
+    startingPoint=0;
+  
+    for(let i=0;i<sourceArray.length;i++){
+        startingPoint= fun(sourceArray[i],startingPoint);
+    }
+    return startingPoint;
+
+}
